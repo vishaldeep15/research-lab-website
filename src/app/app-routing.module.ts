26771234b0694ec0ed93f67ce5da7428838f8848
vishaldeep15 <visdhiman15@gmail.com>
2019-08-25 16:26:36 -0500

    Add components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { TeamComponent } from './team/team.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'research', component: ResearchComponent },
+  { path: 'publications', component: PublicationsComponent },
+  { path: 'team', component: TeamComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResearchComponent,
+    PublicationsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,

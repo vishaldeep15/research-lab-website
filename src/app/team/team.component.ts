148445c6cfad91e732e8dffa16f125269ac0694d
Vishal Deep <vdeep@iastate.edu>
2021-01-01 22:12:59 -0600

    Add profile pics

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -23,8 +23,8 @@ export class TeamComponent implements OnInit {
       { 
         name : "Vishal Deep",
         designation : "Ph.D. Candidate",
-        imagePath : "assets/images/Male_Business_Formal.png",
-        address: "210 Durham 613 Morrill Rd. Ames, IA 50011",
+        imagePath : "assets/images/vishal_profile.jpg",
+        address: "310 Durham 613 Morrill Rd. Ames, IA 50011",
         phone: "559-475-3777",
         email: "vdeep@iastate.edu",
         profileDescription : "Vishal Deep is currently a third-year Ph.D. student in Computer Engineering at Iowa State University. He received his M.S. degree from California State University, Fresno. His broad research interests include computer architecture, embedded systems, and Internet of Things (IoT). He has a particular interest in developing computational and communication primitives for batteryless and intermittently powered devices. "
@@ -32,20 +32,20 @@ export class TeamComponent implements OnInit {
       { 
         name : "Rohit Sahu",
         designation : "Ph.D. Candidate",
-        imagePath : "assets/images/Male_Business_Formal.png",
-        address: "321 Durham 613 Morrill Rd. Ames, IA 50011",
-        phone: "515-294-2158",
-        email: "duwe@iastate.edu",
-        profileDescription : "Hello! I am Rohit."
+        imagePath : "assets/images/rohit_profile.jpg",
+        address: "310 Durham 613 Morrill Rd. Ames, IA 50011",
+        phone: "",
+        email: "rashu@iastate.edu",
+        profileDescription : "Rohit Sahu received his Bachelor's' degree in Electronics and Communication Engineering (ECE) from Institute of Technology, India. He is currently pursuing Ph.D. in the Electrical and Computer (ECpE) department at Iowa State University. His research interests include Edge Machine Learning (Edge ML), computer architecture and intermittent devices. Currently, he is focusing on enabling machine learning on severely resource-constrained devices."
       },
       { 
         name : "Vishak Narayanan",
-        designation : "M.S. Student",
-        imagePath : "assets/images/Male_Business_Formal.png",
-        address: "321 Durham 613 Morrill Rd. Ames, IA 50011",
-        phone: "515-294-2158",
-        email: "duwe@iastate.edu",
-        profileDescription : "Hello! I am Vishak."
+        designation : "Ph.D. Student",
+        imagePath : "assets/images/vishakn_profile.jpg",
+        address: "310 Durham 613 Morrill Rd. Ames, IA 50011",
+        phone: "",
+        email: "vishak@iastate.edu",
+        profileDescription : "Vishak is a graduate student presently doing PhD in computer engineering. He has completed bachelor's degree in electronics and communication from SCMS school of engineering and technology, India in 2017. His research interests include computer architecture, VLSI and embedded systems."
       },
       { 
         name : "Joyesh Philip",
